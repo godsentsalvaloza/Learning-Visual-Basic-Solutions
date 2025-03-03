4dd165cc2ec6b46b39cd4055ba7529b0282e01c9
Godsent <85384933+godsentsalvaloza@users.noreply.github.com>
2025-03-03 20:48:01 +0800

    Until tutorial 1

---
+++ #1 Hello World/BasicTutorial/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btn_start = New Button()
+        SuspendLayout()
+        ' 
+        ' btn_start
+        ' 
+        btn_start.BackColor = Color.FromArgb(CByte(255), CByte(192), CByte(192))
+        btn_start.Location = New Point(197, 112)
+        btn_start.Name = "btn_start"
+        btn_start.Size = New Size(188, 85)
+        btn_start.TabIndex = 0
+        btn_start.Text = "Press Here"
+        btn_start.UseVisualStyleBackColor = False
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(600, 340)
+        Controls.Add(btn_start)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents btn_start As Button
+
+End Class
---
+++ #1 Hello World/BasicTutorial/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub btn_start_Click(sender As Object, e As EventArgs) Handles btn_start.Click
+
+
+        MsgBox("Hello, World!")
+        MsgBox("I wish you a good day~")
+
+
+    End Sub
+End Class
